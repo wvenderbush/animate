53fc8c9a328b5373bf3b0049a9624c7251b9f5c6
Winston Venderbush <winston@Winstons-MacBook-Pro.local>
2017-02-16 15:00:55 -0500

    commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+                     
+var clearBut = document.getElementById("clear");
+clearBut.addEventListener("click", function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+var tick = 50;
+var flag = 0;
+
+var circle = function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	console.log("circle drawn")
+	ctx.fillStyle = "#0000ff";
+	ctx.beginPath();
+	ctx.arc(tick, 100, 50, 0, 2*Math.PI)
+	ctx.fill();
+	ctx.stroke();
+	if (flag = 0){
+		console.log("moving right");
+		tick++;
+	}
+	if (flag = 1){
+		console.log("moving left");
+		tick--;
+	}
+	if (tick == canvas.width - 50){
+		console.log("flag is 1");
+		flag = 1;
+		tick--;
+	}
+	if (tick = 49){
+		console.log("flag is 0");
+		flag = 0;
+		tick++;
+	}
+
+	window.requestAnimationFrame( circle );
+};
+
+//window.requestAnimationFrame( circle );
+
+//circle();
+
+canvas.addEventListener("click", circle)
+
+
